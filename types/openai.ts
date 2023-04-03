2d7ac9dfe25f854550aff8690ea039e7b3d630a9
Yi Su <nuance@gmail.com>
2023-04-02 20:01:29 -0700

    fix model size

--- types/openai.ts
+++ types/openai.ts
@@ -10,14 +10,14 @@ export enum OpenAIModelID {
   GPT_4 = 'gpt-4',
   LLAMA_7B = 'llama-7b',
   LLAMA_13B = 'llama-13b',
-  LLAMA_30B = 'llama-30b',
+  LLAMA_33B = 'llama-33b',
   LLAMA_65B = 'llama-65b',
 }
 
 export const LlamaModels = [
   { id: OpenAIModelID.LLAMA_7B, name: "LLAMA-7B" },
   { id: OpenAIModelID.LLAMA_13B, name: "LLAMA-13B" },
-  { id: OpenAIModelID.LLAMA_30B, name: "LLAMA-30B" },
+  { id: OpenAIModelID.LLAMA_33B, name: "LLAMA-33B" },
   { id: OpenAIModelID.LLAMA_65B, name: "LLAMA-65B" },
 ];
 
@@ -55,9 +55,9 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     maxLength: 24000,
     tokenLimit: 8000,
   },
-  [OpenAIModelID.LLAMA_30B]: {
-    id: OpenAIModelID.LLAMA_30B,
-    name: 'LLAMA-30B',
+  [OpenAIModelID.LLAMA_33B]: {
+    id: OpenAIModelID.LLAMA_33B,
+    name: 'LLAMA-33B',
     maxLength: 24000,
     tokenLimit: 8000,
   },
