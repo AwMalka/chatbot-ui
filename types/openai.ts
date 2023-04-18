f9d3e74966eba18e03def0740313b0e93ca2e5ea
AwMalka <awmalka@gmail.com>
2023-04-19 02:45:44 +0300

    Update openai.ts

--- types/openai.ts
+++ types/openai.ts
@@ -1,6 +1,7 @@
 export interface OpenAIModel {
   id: string;
   name: string;
+
   maxLength: number; // maximum length of a message
   tokenLimit: number;
 }
@@ -8,17 +9,28 @@ export interface OpenAIModel {
 export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo',
   GPT_4 = 'gpt-4',
-  LLAMA_7B = 'llama-7b',
-  LLAMA_13B = 'llama-13b',
-  LLAMA_33B = 'llama-33b',
-  LLAMA_65B = 'llama-65b',
+  T7910_7B = '7b',
+  T7910_7B_native = '7b-native',
+  T7910_vicuna_13B = 'vicuna-13b',
+  T7910_gpt4all = 'gpt4all',
+  T7910_gpt4all_lora = 'gpt4all-lora',
+  T7910_dolly_v2_12b = 'dolly-v2-12b',
+  T7910_alpaca_7b = 'alpaca-7b',
+  T7910_alpaca_13b = 'alpaca-13b',
+  T7910_alpaca_30b = 'alpaca-30',
+
 }
 
 export const LlamaModels = [
-  { id: OpenAIModelID.LLAMA_7B, name: "LLAMA-7B" },
-  { id: OpenAIModelID.LLAMA_13B, name: "LLAMA-13B" },
-  { id: OpenAIModelID.LLAMA_33B, name: "LLAMA-33B" },
-  { id: OpenAIModelID.LLAMA_65B, name: "LLAMA-65B" },
+  { id: OpenAIModelID.T7910_7B, name: "T7910_7B" },
+  { id: OpenAIModelID.T7910_7B_native, name: "T7910_7B_native" },
+  { id: OpenAIModelID.T7910_vicuna_13B, name: "T7910_vicuna_13B" },
+  { id: OpenAIModelID.T7910_gpt4all, name: "T7910_gpt4all" },
+  { id: OpenAIModelID.T7910_gpt4all_lora, name: "T7910_gpt4all_lora" },
+  { id: OpenAIModelID.T7910_dolly_v2_12b, name: "T7910_dolly_v2_12b" },
+  { id: OpenAIModelID.T7910_alpaca_7b, name: "T7910_alpaca_7b" },
+  { id: OpenAIModelID.T7910_alpaca_13b, name: "T7910_alpaca_13b" },
+  { id: OpenAIModelID.T7910_alpaca_30b, name: "T7910_alpaca_30b" },
 ];
 
 const LlamaModelSet = new Set(LlamaModels.map(item => item.id.valueOf()));
@@ -42,29 +54,60 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     name: 'GPT-4',
     maxLength: 24000,
     tokenLimit: 8000,
+  },  
+  [OpenAIModelID.T7910_7B]: {
+    id: OpenAIModelID.T7910_7B,
+    name: 'GPT-3.5',
+    maxLength: 12000,
+    tokenLimit: 4000,
   },
-  [OpenAIModelID.LLAMA_7B]: {
-    id: OpenAIModelID.LLAMA_7B,
-    name: 'LLAMA-7B',
+  [OpenAIModelID.T7910_7B_native]: {
+    id: OpenAIModelID.T7910_7B_native,
+    name: 'GPT-4',
     maxLength: 24000,
     tokenLimit: 8000,
   },
-  [OpenAIModelID.LLAMA_13B]: {
-    id: OpenAIModelID.LLAMA_13B,
-    name: 'LLAMA-13B',
+  [OpenAIModelID.T7910_vicuna_13B]: {
+    id: OpenAIModelID.T7910_vicuna_13B,
+    name: 'T7910_vicuna_13B',
+    maxLength: 24000,
+    tokenLimit: 8000,
+  },  
+  [OpenAIModelID.T7910_gpt4all]: {
+    id: OpenAIModelID.T7910_gpt4all,
+    name: 'T7910_gpt4all',
+    maxLength: 24000,
+    tokenLimit: 8000,
+  },  
+  [OpenAIModelID.T7910_gpt4all_lora]: {
+    id: OpenAIModelID.T7910_gpt4all_lora,
+    name: 'T7910_gpt4all_lora',
+    maxLength: 24000,
+    tokenLimit: 8000,
+  },  
+  [OpenAIModelID.T7910_dolly_v2_12b]: {
+    id: OpenAIModelID.T7910_dolly_v2_12b,
+    name: 'T7910_dolly_v2_12b',
+    maxLength: 24000,
+    tokenLimit: 8000,
+  },  
+  [OpenAIModelID.T7910_alpaca_7b]: {
+    id: OpenAIModelID.T7910_alpaca_7b,
+    name: 'T7910_alpaca_7b',
+    maxLength: 24000,
+    tokenLimit: 8000,
+  },  
+  [OpenAIModelID.T7910_alpaca_13b]: {
+    id: OpenAIModelID.T7910_alpaca_13b,
+    name: 'T7910_alpaca_13b',
+    maxLength: 24000,
+    tokenLimit: 8000,
+  },  
+  [OpenAIModelID.T7910_alpaca_30b]: {
+    id: OpenAIModelID.T7910_alpaca_30b,
+    name: 'T7910_alpaca_30b',
     maxLength: 24000,
     tokenLimit: 8000,
   },
-  [OpenAIModelID.LLAMA_33B]: {
-    id: OpenAIModelID.LLAMA_33B,
-    name: 'LLAMA-33B',
-    maxLength: 24000,
-    tokenLimit: 8000,
-  },
-  [OpenAIModelID.LLAMA_65B]: {
-    id: OpenAIModelID.LLAMA_65B,
-    name: 'LLAMA-65B',
-    maxLength: 24000,
-    tokenLimit: 8000,
-  },
+
 };
